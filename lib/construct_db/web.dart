aeeb916cf2e6e0903907b0a7bf6ad94373a03388
Britannio <britanniojarrett@gmail.com>
2021-10-14 23:48:51 +0100

    setup web worker

--- lib/construct_db/web.dart
+++ lib/construct_db/web.dart
@@ -1,8 +1,17 @@
+import 'dart:html';
+
+import 'package:drift/remote.dart';
 import 'package:drift/web.dart';
 import 'package:drift_safari_demo/database.dart';
 
 Future<MyDatabase> constructDb() async {
-  return MyDatabase(
-    WebDatabase.withStorage(await DriftWebStorage.indexedDbIfSupported('db')),
-  );
+  if (await DriftWebStorage.supportsIndexedDb(inWebWorker: true)) {
+    final worker = SharedWorker('worker.dart.js');
+    final connection = remote(worker.port!.channel());
+    return MyDatabase.connect(connection);
+  } else {
+    return MyDatabase(
+      WebDatabase.withStorage(await DriftWebStorage.indexedDbIfSupported('db')),
+    );
+  }
 }
